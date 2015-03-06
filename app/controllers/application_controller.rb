c81cf632c6bd36762d94b2f920dae0b7c03e2103
stephaniecoleman <sproutwingsandfly@gmail.com>
2015-03-06 16:39:56 -0600

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def index
+  	render text: "Gonna be a website one day!"
+  end
+
+
+
+
+end

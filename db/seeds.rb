ea32431955ef28f9f45a31e3710083cfd1dd55d1
stephaniecoleman <sproutwingsandfly@gmail.com>
2015-04-30 12:00:41 -0400

    Create seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: "Steph")
+User.create(name: "Betsy")
+
+Musician.create(name: "Doc Boggs")
+Musician.create(name: "Tommy Jarrell")
+Musician.create(name: "Roscoe Holcomb")
+Musician.create(name: "Hazel Dickens")
+Musician.create(name: "Marcus Martin")
+
+Following.create(fan_id: 1, musician_id: 1)
+Following.create(fan_id: 1, musician_id: 2)
+Following.create(fan_id: 1, musician_id: 3)
+Following.create(fan_id: 1, musician_id: 4)
+
+Following.create(fan_id: 2, musician_id: 2)
+Following.create(fan_id: 2, musician_id: 3)
+Following.create(fan_id: 2, musician_id: 4)
+Following.create(fan_id: 2, musician_id: 5)
+
+Plink.create(text: "I'm Tommy Jarrell, and I love Doc Boggs", plink_author_id: 2, plink_receiver_id: 1)
+Plink.create(text: "I'm Tommy Jarrell, and I love Roscoe Holcomb", plink_author_id: 2, plink_receiver_id: 3)
+Plink.create(text: "I'm Tommy Jarrell, and I love Hazel Dickens", plink_author_id: 2, plink_receiver_id: 4)
+
+Plink.create(text: "I'm Hazel Dickens, and I love Tommy Jarrell", plink_author_id: 4, plink_receiver_id: 2)
+Plink.create(text: "I'm Hazel Dickens, and I love Doc Boggs", plink_author_id: 4, plink_receiver_id: 1)
